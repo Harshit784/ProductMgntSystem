5780794efc0c8b7cdc405197926bd217fbea9164
Harshit <70321537+Harshit784@users.noreply.github.com>
2022-03-15 18:29:16 +0530

    Update ProductOperations.cs

--- ServiceLayer/ProductOperations.cs
+++ ServiceLayer/ProductOperations.cs
@@ -20,6 +20,12 @@ namespace ServiceLayer
             _productContext.Add<ProductService>(product);
             _productContext.SaveChanges();
         }
+        
+        public void UpdateProduct(ProductService product)
+        {
+            _productContext.Update<ProductService>(product);
+            _productContext.SaveChanges();
+        }
 
         public void DeleteProduct(ProductService product)
         {
@@ -36,10 +42,6 @@ namespace ServiceLayer
             return _productContext.Set<ProductService>().ToList();
         }
 
-        public void UpdateProduct(ProductService product)
-        {
-            _productContext.Update<ProductService>(product);
-            _productContext.SaveChanges();
-        }
+        
     }
 }
