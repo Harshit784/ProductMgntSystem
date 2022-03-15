fbee57562d1c6b6137858403008f911b7094884c
Harshit784 <70321537+Harshit784@users.noreply.github.com>
2022-03-15 23:38:09 +0530

    Customer Details

---
+++ RepositoryLayer/CustomerContext.cs
@@ -0,0 +1,17 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryLayer
+{
+    public class CustomerContext:DbContext
+    {
+        public CustomerContext(DbContextOptions options) : base(options) { }
+        DbSet<CustomerContext> Customers { get; set; }
+
+    }
+}
---
+++ ServiceLayer/ICustomerOperations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DomainLayer.Models;
+
+namespace ServiceLayer
+{
+    public interface ICustomerOperations
+    {
+        /*IList<Customer> GetCustomerDetails();
+        void AddCustomer(Customer customer);
+        void UpdateCustomer(Customer customer);
+        void DeleteCustomer(Customer customer);*/
+    }
+}
