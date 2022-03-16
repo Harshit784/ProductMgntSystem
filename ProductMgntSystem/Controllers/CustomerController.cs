f08ce325b87f1505a6733bd321acc111d438132f
Harshit784 <70321537+Harshit784@users.noreply.github.com>
2022-03-16 18:39:56 +0530

    Customer Controller

---
+++ ProductMgntSystem/Controllers/CustomerController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMgntSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ProductOperations _productOperations;
+
+        public CustomerController(ProductOperations productOperations)
+        {
+            _productOperations = productOperations;
+        }
+        [HttpGet(nameof(GetProductDetails))]
+
+        public IActionResult GetProductDetails()
+        {
+            var products = _productOperations.GetProductDetails();
+            try
+            {
+                if (products == null)
+                {
+                    return NotFound();
+                }
+                return Ok(products);
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
